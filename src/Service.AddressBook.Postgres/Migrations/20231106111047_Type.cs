02844a1cdca3b1dfc53fda0d23973a5093ad317b
Pavlo Pylypenko <pavel.p@smplt.net>
2023-11-06 13:10:54 +0200

    migration

---
+++ src/Service.AddressBook.Postgres/Migrations/20231106111047_Type.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.AddressBook.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class Type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BankCountry",
+                schema: "addressbook",
+                table: "addressbook",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                schema: "addressbook",
+                table: "addressbook",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IbanType",
+                schema: "addressbook",
+                table: "addressbook",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankCountry",
+                schema: "addressbook",
+                table: "addressbook");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                schema: "addressbook",
+                table: "addressbook");
+
+            migrationBuilder.DropColumn(
+                name: "IbanType",
+                schema: "addressbook",
+                table: "addressbook");
+        }
+    }
+}
